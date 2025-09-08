75c377d16559eb92ec4a42524dbfdf57ed2625d1
tokurah ibrahim <tokurah10@gmail.com>
2025-09-08 18:09:29 +0100

    made changes

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,7 +1,7 @@
 number1 = 10
 number2 = 5
 add = number1 + number2
-difference = number1 - number
+difference = number1 - number2
 product = number1 * number2
 print(f"Addition of {number1} and {number2} is",add)
 print(f"Subtraction of {number1} and {number2} is",difference)
