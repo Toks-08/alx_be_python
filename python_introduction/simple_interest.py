1c0582f819721f41a1c15eb2ef7adcdf30d198f8
tokurah ibrahim <tokurah10@gmail.com>
2025-09-09 12:06:23 +0100

    s

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,9 +1,5 @@
 principal = 1000
 rate=  0.05
 time = 3
-P = principal
-R = rate
-T = time
-I = P * R * T
-interest = I
-print(f"The simple interest is:",interest)
+interest = principal * rate * time
+print("The simple interest is:",interest)
