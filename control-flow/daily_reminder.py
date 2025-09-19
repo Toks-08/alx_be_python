93c60db25ae992f7f4f283df62b449a0de32eb93
tokurah ibrahim <tokurah10@gmail.com>
2025-09-19 15:58:22 +0100

    remainder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,23 +4,23 @@ time_bound = input("Is it time-bound? (yes/no):").lower()
 match priority:
     case "high":
         if time_bound == 'yes':
-            print(f"{task} is a {priority} priority task that requires immediate attention today!")
+            print(f"Remainder:{task} is a {priority} priority task that requires immediate attention today!")
         elif time_bound =="no":
-            print(f"{task} is a {priority} priority task. Consider completing it when you have free time.!")
+            print(f"Remainder:{task} is a {priority} priority task. Consider completing it when you have free time.!")
         else:
             print(f"please indicate if {task} is time based with yes/no")
     case "medium":
         if time_bound == 'yes':
-            print(f"{task} is a {priority} priority task that requires immediate attention today!")
+            print(f"Remainder:{task} is a {priority} priority task that requires immediate attention today!")
         elif time_bound == "no":
-            print(f"{task} is a {priority} priority task. Consider completing it when you have free time.!")
+            print(f"Remainder:{task} is a {priority} priority task. Consider completing it when you have free time.!")
         else:
             print(f"please indicate if {task} is time based with yes/no")
     case "low":
             if time_bound == 'yes':
-                print(f"{task} is a {priority} priority task that requires immediate attention today!")
+                print(f"Remainder:{task} is a {priority} priority task that requires immediate attention today!")
             elif time_bound == "no":
-                print(f"{task} is a {priority} priority task. Consider completing it when you have free time.!")
+                print(f"Remainder:{task} is a {priority} priority task. Consider completing it when you have free time.!")
             else:
                 print(f"please indicate if {task} is time based with yes/no")
 
